808f8aeb55cf0d48ab9655ef11e08d29aa11b48a
Joseph Gichohi <josephgichohi322@gmail.com>
2024-04-18 16:50:56 +0300

    start of the application

--- src/main/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import main_view
-
-
-urlpatterns = [
-    path('', main_view, name = "main" )
-]
--- src/main/views.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-# Create your views here.
-def main_view(request):
-    return render(request, "views/main.html", {"name": "Automax"})
