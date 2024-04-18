d7c8f15d3f8777a5be04171625d994ab40e052e4
Joseph Gichohi <josephgichohi322@gmail.com>
2024-04-18 14:21:02 +0300

    Boostrap package

--- src/main/views.py
+++ src/main/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def main_view(request):
-    return HttpResponse("<h1>Welcome to automax app!!</h1>")
+    return render(request, "views/main.html", {"name": "Automax"})
