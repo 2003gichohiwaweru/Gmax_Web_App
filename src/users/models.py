4f616340c7ec8181b5a0afbdb46ed8182eb4b218
Joseph Gichohi <josephgichohi322@gmail.com>
2024-04-19 20:58:01 +0300

    first commit

--- src/users/models.py
+++ src/users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User 
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    photo = models.ImageField(null=True, upload_to=None, height_field=None, width_field=None, max_length=None)
+    bio = models.CharField(max_length=140, blank=True)
+    phone_number = models.CharField(max_length=12, blank=True)
