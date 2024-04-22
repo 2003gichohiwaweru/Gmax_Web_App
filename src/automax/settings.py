ea8ca158c40c80d3ac2308a41aa87ccf10c84a46
Joseph Gichohi <josephgichohi322@gmail.com>
2024-04-22 15:45:22 +0300

    updates on the media file

--- src/automax/settings.py
+++ src/automax/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -125,6 +126,8 @@ STATICFILES_DIRS = [
     BASE_DIR / 'main/static',  # Assuming your static files are located in the 'static' directory in the project root
 ]
 
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- src/users/models.py
+++ src/users/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User 
 from localflavor.us.models import USStateField, USZipCodeField
+from .utils import user_directory_path
+
 
 class Location(models.Model):
     address_1 = models.CharField(max_length=128, blank=True)
@@ -14,7 +16,7 @@ class Location(models.Model):
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-    photo = models.ImageField(null=True, upload_to=None, height_field=None, width_field=None, max_length=None)
+    photo = models.ImageField(null=True, upload_to=user_directory_path, height_field=None, width_field=None, max_length=None)
     bio = models.CharField(max_length=140, blank=True)
     phone_number = models.CharField(max_length=12, blank=True)
     location = models.OneToOneField(Location, on_delete=models.SET_NULL, null=True, related_name='profile')
