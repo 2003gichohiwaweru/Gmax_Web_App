e53d2800553f977a126f3d261766579894b987a6
Joseph Gichohi <josephgichohi322@gmail.com>
2024-05-24 14:36:50 +0300

    like_listing_view

--- src/main/urls.py
+++ src/main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import main_view, home_view, list_view,edit_view, listing_view
+from .views import main_view, home_view, list_view,edit_view, listing_view, like_listing_view
 
 urlpatterns = [
     path('', main_view, name = 'main' ),
@@ -7,5 +7,7 @@ urlpatterns = [
     path('list/', list_view, name= 'list' ),
     path('listing/<str:id>/', listing_view, name= 'listing' ),
     path('listing/<str:id>/edit', edit_view, name= 'edit' ),
+     path('listing/<str:id>/like', like_listing_view, name= 'like' )
+
 
 ]
