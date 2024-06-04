bef9c196f3b187980efeab230a4e6b453397c27d
Joseph Gichohi <josephgichohi322@gmail.com>
2024-06-04 16:23:25 +0300

    like icon updates

--- src/main/urls.py
+++ src/main/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('list/', list_view, name= 'list' ),
     path('listing/<str:id>/', listing_view, name= 'listing' ),
     path('listing/<str:id>/edit/', edit_view, name= 'edit' ),
-    path('listing/<str:id>/like/', like_listing_view, name= 'liked_listing' ),
+    path('listing/<str:id>/like/', like_listing_view, name= 'like_listing' ),
     
 
 
