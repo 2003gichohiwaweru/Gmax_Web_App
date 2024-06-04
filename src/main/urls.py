93dbd84c3bd25c3c0de0d0bb4b9e850faba0e0de
Joseph Gichohi <josephgichohi322@gmail.com>
2024-06-04 14:15:18 +0300

    Updates

--- src/main/urls.py
+++ src/main/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
     path('home/', home_view, name = 'home'),
     path('list/', list_view, name= 'list' ),
     path('listing/<str:id>/', listing_view, name= 'listing' ),
-    path('listing/<str:id>/edit', edit_view, name= 'edit' ),
-     path('listing/<str:id>/like', like_listing_view, name= 'like' )
+    path('listing/<str:id>/edit/', edit_view, name= 'edit' ),
+    path('listing/<str:id>/like/', like_listing_view, name= 'like_listing' )
 
 
 ]
